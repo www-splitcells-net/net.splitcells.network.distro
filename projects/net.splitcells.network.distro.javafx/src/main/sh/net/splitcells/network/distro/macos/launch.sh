28892eae5977a9434ce95cab5c6477f03c0d926b
Mārtiņš Avots <martins.avots@splitcells.net>
2024-02-22 20:15:20 +0100

    #252 Create zip for MacOS app
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.network.distro.javafx/src/main/sh/net/splitcells/network/distro/macos/launch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+set -e
+jre/Contents/Home/bin/java \
+  --module-path "./javafx-lib" --add-modules javafx.controls,javafx.web \
+  -cp './deployable-jars/*' net.splitcells.network.distro.javafx.JavaFxGuiLauncher
