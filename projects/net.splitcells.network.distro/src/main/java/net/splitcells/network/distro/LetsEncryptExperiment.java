4af20237404b07e09cba46167f5109006be37ef2
Mārtiņš Avots <martins.avots@splitcells.net>
2024-04-19 23:48:02 +0200

    #c11 Try creating CA account
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.network.distro/src/main/java/net/splitcells/network/distro/LetsEncryptExperiment.java
+++ projects/net.splitcells.network.distro/src/main/java/net/splitcells/network/distro/LetsEncryptExperiment.java
@@ -15,33 +15,53 @@
  */
 package net.splitcells.network.distro;
 
+import net.bytebuddy.implementation.bytecode.Throw;
+import net.splitcells.dem.resource.Paths;
+import net.splitcells.dem.resource.communication.Domsole;
+import net.splitcells.dem.resource.communication.log.LogLevel;
+import net.splitcells.dem.resource.communication.log.Logs;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.shredzone.acme4j.Account;
+import org.shredzone.acme4j.AccountBuilder;
 import org.shredzone.acme4j.Authorization;
 import org.shredzone.acme4j.Certificate;
 import org.shredzone.acme4j.Session;
 import org.shredzone.acme4j.Status;
+import org.shredzone.acme4j.util.KeyPairUtils;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyPair;
+import java.security.Security;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+import static net.splitcells.dem.lang.perspective.PerspectiveI.perspective;
 import static net.splitcells.dem.utils.ExecutionException.executionException;
 import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 
 public class LetsEncryptExperiment {
     public static void main(String... args) {
-        System.out.println(new LetsEncryptExperiment().certificate("live.splitcells.net"));
+        System.out.println(new LetsEncryptExperiment("contacts@splitcells.net").certificate("live.splitcells.net"));
     }
 
-    private LetsEncryptExperiment() {
-        throw notImplementedYet();
+    private final String sessionUrl = "acme://letsencrypt.org";
+    private final String email;
+    private final Path userKeyPairPath = Paths.userHome("acme-user-key-pair");
+    private final Path domainKeyPairPath = Paths.userHome("acme-domain-key-pair");
+
+    private LetsEncryptExperiment(String emailArg) {
+        Security.addProvider(new BouncyCastleProvider());
+        email = emailArg;
     }
 
     public Certificate certificate(String domain) {
         try {
             final var userKeyPair = userKeyPair();
             final var domainKeyPair = domainKeyPair();
-            final var session = new Session("acme://letsencrypt.org");
+            final var session = new Session(sessionUrl);
             final var account = account(session, userKeyPair);
             return certificate(domain, account, domainKeyPair);
         } catch (Throwable t) {
@@ -76,14 +96,60 @@ public class LetsEncryptExperiment {
     }
 
     public KeyPair userKeyPair() {
-        throw notImplementedYet();
+        if (Files.exists(userKeyPairPath)) {
+            try (FileReader fileReader = new FileReader(userKeyPairPath.toFile())) {
+                return KeyPairUtils.readKeyPair(fileReader);
+            } catch (Throwable t) {
+                throw executionException(t);
+            }
+        }
+        try (FileWriter fw = new FileWriter(userKeyPairPath.toFile())) {
+            final var newUserKeypair = KeyPairUtils.createKeyPair();
+            KeyPairUtils.writeKeyPair(newUserKeypair, fw);
+            return newUserKeypair;
+        } catch (Throwable t) {
+            throw executionException(t);
+        }
     }
 
+    /**
+     * TODO Persist and load generated domain key pairs.
+     *
+     * @return
+     */
     public KeyPair domainKeyPair() {
-        throw notImplementedYet();
+        if (Files.exists(domainKeyPairPath)) {
+            try (FileReader fileReader = new FileReader(domainKeyPairPath.toFile())) {
+                return KeyPairUtils.readKeyPair(fileReader);
+            } catch (Throwable t) {
+                throw executionException(t);
+            }
+        }
+        try (FileWriter fw = new FileWriter(domainKeyPairPath.toFile())) {
+            final var newUserKeypair = KeyPairUtils.createKeyPair(4096);
+            KeyPairUtils.writeKeyPair(newUserKeypair, fw);
+            return newUserKeypair;
+        } catch (Throwable t) {
+            throw executionException(t);
+        }
     }
 
     public Account account(Session session, KeyPair userKeyPair) {
-        throw notImplementedYet();
+        try {
+            var tos = session.getMetadata().getTermsOfService();
+
+            final var accountBuilder = new AccountBuilder()
+                    .agreeToTermsOfService()
+                    .useKeyPair(userKeyPair);
+            accountBuilder.addEmail(email);
+            Account account = accountBuilder.create(session);
+            Logs.logs().append(perspective("Created a new account for generating public certificates via ACME.")
+                            .withProperty("account location", account.getLocation().toString())
+                            .withProperty("email", email)
+                    , LogLevel.INFO);
+            return account;
+        } catch (Throwable t) {
+            throw executionException("Could not generate account for ACME.", t);
+        }
     }
 }
