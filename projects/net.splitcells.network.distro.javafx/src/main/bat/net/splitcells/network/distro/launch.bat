bbca1c747db30e45c65e46fa867bee5938171ec8
Mārtiņš Avots <martins.avots@splitcells.net>
2025-07-06 09:39:32 +0200

    Add missing license data #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.network.distro.javafx/src/main/bat/net/splitcells/network/distro/launch.bat
+++ projects/net.splitcells.network.distro.javafx/src/main/bat/net/splitcells/network/distro/launch.bat
@@ -1,1 +1,3 @@
+:: SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+:: SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
 jre\bin\java.exe --module-path ".\javafx-lib" --add-modules javafx.controls,javafx.web -cp ".\deployable-jars\*" net.splitcells.network.distro.javafx.JavaFxGuiLauncher
