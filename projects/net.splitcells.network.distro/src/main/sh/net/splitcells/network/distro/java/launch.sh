5ee0df139e660540b58cfe523b8f0dacf43e7431
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-09 15:45:54 +0200

    Copy build source to new project #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.network.distro/src/main/sh/net/splitcells/network/distro/java/launch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+set -e
+jre/bin/java -cp './deployable-jars/*' net.splitcells.network.distro.java.DistroGuiLauncher
